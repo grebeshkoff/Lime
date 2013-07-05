af503a8c516bfaf83407d623a2b3e3fd83fd0481
grebeshkoff <grebeshkoff@gmail.com>
2013-07-05 16:46:50 +0300

    45623476

--- Lime/Data/Source/LimeDatabase.Operations.cs
+++ Lime/Data/Source/LimeDatabase.Operations.cs
@@ -29,6 +29,79 @@ namespace Lime.Data.Source
 #endregion
 
 
+#region * Persons Methods *
+
+        public Person GetPersonById(int id)
+        {
+            return (from p in Persons
+                    where p.Id == id
+                    select p).First();
+        }
+
+        public Person GetPersonByCode(string code)
+        {
+            return (from p in Persons
+                    where p.Code == code
+                    select p).First();
+        }
+
+        public int AddPerson(Person person)
+        {
+            return SetCommand(@"
+                        INSERT INTO Persons
+                            ( PersonCode,  PersonFullName,  PersonGender)
+                        VALUES
+                            ( @PersonCode,  @PersonFullName,  @PersonGender)
+                        SELECT Cast(SCOPE_IDENTITY() as int)",
+                        CreateParameters(person))
+                    .ExecuteScalar<int>();
+        }
+
+        public int UpdatePerson(Person person)
+        {
+            return SetCommand(@"
+                        UPDATE
+                            Persons
+                        SET
+                            PersonCode   = @PersonCode,
+                            PersonFullName  = @PersonFullName,
+                            PersonGender = @PersonGender
+                        WHERE
+                            PersonId = @PersonId",
+            CreateParameters(person)).ExecuteNonQuery();
+        }
+
+        public void DeletePerson(int id)
+        {
+
+            var q = (from p in Parameters
+                     where p.PersonId == id && p.Type == ParameterType.Lookup
+                     select p);
+            var peson = GetPersonById(id);
+
+            SetCommand("DELETE FROM Persons WHERE PersonId = @id",
+                Parameter("@id", id))
+                    .ExecuteNonQuery();
+
+
+            if (q.Any())
+            {
+                var lookupParam = q.First();
+
+                SetCommand("DELETE FROM ParamValues WHERE ParamId = @id",
+                           Parameter("@id", lookupParam.Id)).ExecuteNonQuery();
+            }
+
+            SetCommand("DELETE FROM Params WHERE ParamPersonId = @id",
+               Parameter("@id", id)).ExecuteNonQuery();
+        }
+
+        public void DeletePerson(Person person)
+        {
+            DeletePerson(person.Id);
+        }
+#endregion
+
 #region * Parameters Methods *
 
         public List<Parameter> GetParametersByPerson(Person person)
